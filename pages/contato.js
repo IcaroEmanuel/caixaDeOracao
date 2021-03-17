447f9c392d5d918e95659bf5bbfc1ae7c5aed8ba
Icaro Emanuel <icaro.emanuel@outlook.com>
2021-03-17 20:48:56 -0300

    Alteração de texto na página sobre e contato

--- pages/contato.js
+++ pages/contato.js
@@ -4,15 +4,7 @@ import Link from 'next/link'
 const Contato = () => {
   return (
     <div>
-      <h1>Contato</h1>
-      <div>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-        <Link href='/sobre'>
-          <a>Sobre</a>
-        </Link>
-      </div>
+      <h1>Em desenvolvimento...</h1>
     </div>
   )
 }
--- pages/sobre.js
+++ pages/sobre.js
@@ -4,15 +4,7 @@ import Link from 'next/link'
 const Sobre = () => {
   return (
     <div>
-      <h1>Aplicativo criado para que mais pessoas possam enviar possam enviar pedidos de oração para que sejam acolhidos e levados a Deus por um grupo de intercessores</h1>
-      <div>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-        <Link href='/Contato'>
-          <a>Contato</a>
-        </Link>
-      </div>
+      <h1>Em desenvolvimento...</h1>
     </div>
   )
 }
